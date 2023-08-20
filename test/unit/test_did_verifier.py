197dfb7717e023c60ade677cf3688d34d8c64191
Boris Resnick <boris@flyver.city>
2023-08-20 11:40:33 +0000

    General DID framework set up

---
+++ test/unit/test_did_verifier.py
@@ -0,0 +1,13 @@
+from jwt_pep import jwt_pep
+
+from pathlib import Path
+
+base_dir = Path(__file__).resolve().parents[2]
+
+
+def did_verifier(config, resource_id, token, dpop):
+    pass
+
+
+def test_did_verifier():
+    pass
