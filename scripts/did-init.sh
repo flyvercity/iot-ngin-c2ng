89f2a60f7cb6047932b013efa05d7cbfba2d8c8d
Boris Resnick <boris@flyver.city>
2023-08-21 08:59:54 +0000

    did: Removed 'self' mode for now

--- scripts/did-init.sh
+++ scripts/did-init.sh
@@ -4,7 +4,6 @@ KEY_TOOL=integration/ppssi/key_tools.py
 JWT_TOOL=integration/ppssi/create_jwt.py
 ISSUER_KEY=docker/core/config/did/issuer.pem
 ISSUER_DID_FILE=docker/core/config/did/issuer.did
-ISSUER_DID_SELF_FILE=docker/core/config/did/issuer.did.self
 SIM_DRONE_ID_KEY=docker/core/config/did/sim-drone-id.pem
 SIM_DRONE_ID_DID_FILE=docker/core/config/did/sim-drone-id.did
 SIM_DRONE_ID_JWT=docker/core/config/did/sim-drone-id.jwt
@@ -19,10 +18,6 @@ echo "Sim drone key generated at $SIM_DRONE_ID_KEY"
 ISSUER_DID=`python $KEY_TOOL show_did --did_method did:key $ISSUER_KEY`
 echo $ISSUER_DID > $ISSUER_DID_FILE
 echo "Issuer DID: $ISSUER_DID"
-
-ISSUER_DID_SELF=`python $KEY_TOOL show_did $ISSUER_KEY`
-echo $ISSUER_DID_SELF > $ISSUER_DID_SELF_FILE
-echo "Issuer DID (self): $ISSUER_DID_SELF"
 
 SIM_DRONE_ID_DID=`python $KEY_TOOL show_did --did_method did:key $SIM_DRONE_ID_KEY`
 echo $SIM_DRONE_ID_DID > $SIM_DRONE_ID_DID_FILE
--- test/unit/test_did_verifier.py
+++ test/unit/test_did_verifier.py
@@ -1,12 +1,10 @@
-import json
 from pathlib import Path
 
-from jwt_pep import jwt_pep  # type: ignore  # noqa: F401
+from c2ng.uas_sim.did_verifier import verify
 
 
 def gen_config(resource_id):
     issuer_did = Path('did/issuer.did').read_text().strip()
-    issuer_did_self = Path('did/issuer.did.self').read_text().strip()
 
     config = {
         'resources': {
@@ -16,10 +14,6 @@ def gen_config(resource_id):
                     'trusted_issuers': {
                         issuer_did: {
                             'issuer_key': issuer_did,
-                            'issuer_key_type': 'did'
-                        },
-                        issuer_did_self: {
-                            'issuer_key': issuer_did_self,
                             'issuer_key_type': 'did'
                         }
                     },
@@ -34,45 +28,10 @@ def gen_config(resource_id):
     return config
 
 
-def did_verifier(config, resource_id, token):
-    if not token:
-        return False, 'No token provided'
-
-    if not (resource := config['resources'].get(resource_id)):
-        return False, 'Resource not found'
-
-    trusted_issuers = resource['authorization']['trusted_issuers']
-    filter = resource['authorization']['filters']
-
-    jwt_pep_obj = jwt_pep()
-
-    jwt_verified, ver_output = jwt_pep_obj.verify_jwt(
-        token=token,
-        trusted_issuers=trusted_issuers,
-        filter=filter
-    )
-
-    if not jwt_verified:
-        return False, f'JWS was not verified ({ver_output})'
-
-    payload = json.loads(ver_output)
-
-    vc = payload['vc']
-    capabilities = vc['credentialSubject']['capabilities']
-    print('CAPS', capabilities)
-
-    if 'CONTROL' not in capabilities[resource_id]:
-        return False, 'CONTROL capability is not present'
-
-    # TODO: Handle expiration
-    # TODO: Handle DPoP
-    return True, None
-
-
 def test_did_verifier():
     resource_id = 'sim-drone-id'
     config = gen_config(resource_id)
     token = Path(f'did/{resource_id}.jwt').read_text().strip()
-    verified, error = did_verifier(config, resource_id, token)
+    verified, error = verify(config, resource_id, token)
     assert verified
     assert not error
